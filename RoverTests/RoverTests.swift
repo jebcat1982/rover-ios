a8c5bf4547fbd0e472fbb9923b29aa5638458d2b
Sean Rucker <sean@rover.io>
2017-04-02 16:07:11 -0400

    Test assemble

--- RoverTests/RoverTests.swift
+++ RoverTests/RoverTests.swift
@@ -9,12 +9,23 @@
 import XCTest
 import Foundation
 
+import RoverEvents
+import RoverFoundation
+import RoverHTTP
+import RoverSync
+import RoverUser
+
 @testable import Rover
 
 class RoverTests: XCTestCase {
 
     func testAssemble() {
         Rover.assemble(accountToken: "foo")
-        let _ = Rover.shared
+        
+        XCTAssertNotNil(Rover.shared.resolve(EventsDataService.self))
+        XCTAssertNotNil(Rover.shared.resolve(EventsManagerService.self))
+        XCTAssertNotNil(Rover.shared.resolve(SyncDataService.self))
+        XCTAssertNotNil(Rover.shared.resolve(UserDataService.self))
+        XCTAssertNotNil(Rover.shared.resolve(UserService.self))
     }
 }
