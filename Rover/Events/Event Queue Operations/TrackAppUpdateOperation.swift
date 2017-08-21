00ca2ad67626ec12a0ed6ea02d4c4ee411800e79
Sean Rucker <sean@rover.io>
2017-08-21 11:39:05 -0400

    Add debug info for app update

--- Rover/Events/Event Queue Operations/TrackAppUpdateOperation.swift
+++ Rover/Events/Event Queue Operations/TrackAppUpdateOperation.swift
@@ -20,18 +20,28 @@ class TrackAppUpdateOperation: Operation {
     }
     
     override func execute(reducer: Reducer, resolver: Resolver, completionHandler: @escaping () -> Void) {
-        let previousVersion = userDefaults.string(forKey: "io.rover.appVersion")
-        let previousBuild = userDefaults.string(forKey: "io.rover.appBuild")
-        
         let context = resolver.currentState.context
         let currentVersion = context.appVersion
         let currentBuild = context.appBuild
         
-        if previousBuild == nil {
+        let currentVersionString = versionString(version: currentVersion, build: currentBuild)
+        delegate?.debug("Current version: \(currentVersionString)", operation: self)
+        
+        let previousVersion = userDefaults.string(forKey: "io.rover.appVersion")
+        let previousBuild = userDefaults.string(forKey: "io.rover.appBuild")
+        
+        let previousVersionString = versionString(version: previousVersion, build: previousBuild)
+        delegate?.debug("Previous version: \(previousVersionString)", operation: self)
+        
+        if previousVersion == nil || previousBuild == nil {
+            delegate?.debug("Previous version not found – first time running app with Rover", operation: self)
+            
             let timestamp = Date()
             let operation = TrackEventOperation(eventName: "App Installed", attributes: nil, timestamp: timestamp)
             addOperation(operation)
         } else if currentVersion != previousVersion || currentBuild != previousBuild {
+            delegate?.debug("Current and previous versions do not match – app has been updated", operation: self)
+            
             var attributes = Attributes()
             
             if let previousVersion = previousVersion {
@@ -45,10 +55,24 @@ class TrackAppUpdateOperation: Operation {
             let timestamp = Date()
             let operation = TrackEventOperation(eventName: "App Updated", attributes: attributes, timestamp: timestamp)
             addOperation(operation)
+        } else {
+            delegate?.debug("Current and previous versions match – nothing to track", operation: self)
         }
         
         userDefaults.set(currentVersion, forKey: "io.rover.appVersion")
         userDefaults.set(currentBuild, forKey: "io.rover.appBuild")
         completionHandler()
+    }
+    
+    func versionString(version: String?, build: String?) -> String {
+        if let version = version {
+            if let build = build {
+                return "\(version) (\(build))"
+            }
+            
+            return "\(version)"
+        }
+        
+        return "n/a"
     }
 }
